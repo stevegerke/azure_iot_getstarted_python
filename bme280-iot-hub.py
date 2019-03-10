8f1abb02403a960797649f42a5ecc4bf47f33215
Steve Gerke <steven.gerke@gmail.com>
2019-03-09 23:44:54 -0500

    initial commit

---
+++ bme280-iot-hub.py
@@ -0,0 +1,260 @@
+#!/usr/bin/python
+#--------------------------------------
+#    ___  ___  _ ____
+#   / _ \/ _ \(_) __/__  __ __
+#  / , _/ ___/ /\ \/ _ \/ // /
+# /_/|_/_/  /_/___/ .__/\_, /
+#                /_/   /___/
+#
+#           bme280.py
+#  Read data from a digital pressure sensor.
+#
+#  Official datasheet available from :
+#  https://www.bosch-sensortec.com/bst/products/all_products/bme280
+#
+# Author : Matt Hawkins
+# Date   : 21/01/2018
+#
+# https://www.raspberrypi-spy.co.uk/
+#
+#
+# March 9, 2019 SG Added Microsoft Azure IoT Hub send event
+# Copyright (c) Microsoft. All rights reserved.
+# Licensed under the MIT license. See LICENSE file in the project root for full license information.
+#
+# IoT Hub requires the following:
+#   pip install azure-iothub-device-client
+#   sudo apt-get install libboost-all-dev
+#
+#--------------------------------------
+import smbus
+import time
+from ctypes import c_short
+from ctypes import c_byte
+from ctypes import c_ubyte
+
+#Microsoft libraries
+import random
+import time
+import sys
+
+DEVICE = 0x76 # Default device I2C address
+
+
+bus = smbus.SMBus(1) # Rev 2 Pi, Pi 2 & Pi 3 uses bus 1
+                     # Rev 1 Pi uses bus 0
+
+def getShort(data, index):
+  # return two bytes from data as a signed 16-bit value
+  return c_short((data[index+1] << 8) + data[index]).value
+
+def getUShort(data, index):
+  # return two bytes from data as an unsigned 16-bit value
+  return (data[index+1] << 8) + data[index]
+
+def getChar(data,index):
+  # return one byte from data as a signed char
+  result = data[index]
+  if result > 127:
+    result -= 256
+  return result
+
+def getUChar(data,index):
+  # return one byte from data as an unsigned char
+  result =  data[index] & 0xFF
+  return result
+
+def readBME280ID(addr=DEVICE):
+  # Chip ID Register Address
+  REG_ID     = 0xD0
+  (chip_id, chip_version) = bus.read_i2c_block_data(addr, REG_ID, 2)
+  return (chip_id, chip_version)
+
+def readBME280All(addr=DEVICE):
+  # Register Addresses
+  REG_DATA = 0xF7
+  REG_CONTROL = 0xF4
+  REG_CONFIG  = 0xF5
+
+  REG_CONTROL_HUM = 0xF2
+  REG_HUM_MSB = 0xFD
+  REG_HUM_LSB = 0xFE
+
+  # Oversample setting - page 27
+  OVERSAMPLE_TEMP = 2
+  OVERSAMPLE_PRES = 2
+  MODE = 1
+
+  # Oversample setting for humidity register - page 26
+  OVERSAMPLE_HUM = 2
+  bus.write_byte_data(addr, REG_CONTROL_HUM, OVERSAMPLE_HUM)
+
+  control = OVERSAMPLE_TEMP<<5 | OVERSAMPLE_PRES<<2 | MODE
+  bus.write_byte_data(addr, REG_CONTROL, control)
+
+  # Read blocks of calibration data from EEPROM
+  # See Page 22 data sheet
+  cal1 = bus.read_i2c_block_data(addr, 0x88, 24)
+  cal2 = bus.read_i2c_block_data(addr, 0xA1, 1)
+  cal3 = bus.read_i2c_block_data(addr, 0xE1, 7)
+
+  # Convert byte data to word values
+  dig_T1 = getUShort(cal1, 0)
+  dig_T2 = getShort(cal1, 2)
+  dig_T3 = getShort(cal1, 4)
+
+  dig_P1 = getUShort(cal1, 6)
+  dig_P2 = getShort(cal1, 8)
+  dig_P3 = getShort(cal1, 10)
+  dig_P4 = getShort(cal1, 12)
+  dig_P5 = getShort(cal1, 14)
+  dig_P6 = getShort(cal1, 16)
+  dig_P7 = getShort(cal1, 18)
+  dig_P8 = getShort(cal1, 20)
+  dig_P9 = getShort(cal1, 22)
+
+  dig_H1 = getUChar(cal2, 0)
+  dig_H2 = getShort(cal3, 0)
+  dig_H3 = getUChar(cal3, 2)
+
+  dig_H4 = getChar(cal3, 3)
+  dig_H4 = (dig_H4 << 24) >> 20
+  dig_H4 = dig_H4 | (getChar(cal3, 4) & 0x0F)
+
+  dig_H5 = getChar(cal3, 5)
+  dig_H5 = (dig_H5 << 24) >> 20
+  dig_H5 = dig_H5 | (getUChar(cal3, 4) >> 4 & 0x0F)
+
+  dig_H6 = getChar(cal3, 6)
+
+  # Wait in ms (Datasheet Appendix B: Measurement time and current calculation)
+  wait_time = 1.25 + (2.3 * OVERSAMPLE_TEMP) + ((2.3 * OVERSAMPLE_PRES) + 0.575) + ((2.3 * OVERSAMPLE_HUM)+0.575)
+  time.sleep(wait_time/1000)  # Wait the required time  
+
+  # Read temperature/pressure/humidity
+  data = bus.read_i2c_block_data(addr, REG_DATA, 8)
+  pres_raw = (data[0] << 12) | (data[1] << 4) | (data[2] >> 4)
+  temp_raw = (data[3] << 12) | (data[4] << 4) | (data[5] >> 4)
+  hum_raw = (data[6] << 8) | data[7]
+
+  #Refine temperature
+  var1 = ((((temp_raw>>3)-(dig_T1<<1)))*(dig_T2)) >> 11
+  var2 = (((((temp_raw>>4) - (dig_T1)) * ((temp_raw>>4) - (dig_T1))) >> 12) * (dig_T3)) >> 14
+  t_fine = var1+var2
+  temperature = float(((t_fine * 5) + 128) >> 8);
+
+  # Refine pressure and adjust for temperature
+  var1 = t_fine / 2.0 - 64000.0
+  var2 = var1 * var1 * dig_P6 / 32768.0
+  var2 = var2 + var1 * dig_P5 * 2.0
+  var2 = var2 / 4.0 + dig_P4 * 65536.0
+  var1 = (dig_P3 * var1 * var1 / 524288.0 + dig_P2 * var1) / 524288.0
+  var1 = (1.0 + var1 / 32768.0) * dig_P1
+  if var1 == 0:
+    pressure=0
+  else:
+    pressure = 1048576.0 - pres_raw
+    pressure = ((pressure - var2 / 4096.0) * 6250.0) / var1
+    var1 = dig_P9 * pressure * pressure / 2147483648.0
+    var2 = pressure * dig_P8 / 32768.0
+    pressure = pressure + (var1 + var2 + dig_P7) / 16.0
+
+  # Refine humidity
+  humidity = t_fine - 76800.0
+  humidity = (hum_raw - (dig_H4 * 64.0 + dig_H5 / 16384.0 * humidity)) * (dig_H2 / 65536.0 * (1.0 + dig_H6 / 67108864.0 * humidity * (1.0 + dig_H3 / 67108864.0 * humidity)))
+  humidity = humidity * (1.0 - dig_H1 * humidity / 524288.0)
+  if humidity > 100:
+    humidity = 100
+  elif humidity < 0:
+    humidity = 0
+
+  return temperature/100.0,pressure/100.0,humidity
+
+
+#---------- Microsoft start ----------
+# Using the Python Device SDK for IoT Hub:
+#   https://github.com/Azure/azure-iot-sdk-python
+# The sample connects to a device-specific MQTT endpoint on your IoT Hub.
+import iothub_client
+# pylint: disable=E0611
+from iothub_client import IoTHubClient, IoTHubClientError, IoTHubTransportProvider, IoTHubClientResult
+from iothub_client import IoTHubMessage, IoTHubMessageDispositionResult, IoTHubError, DeviceMethodReturnValue
+
+# The device connection string to authenticate the device with your IoT hub.
+# Using the Azure CLI:
+# az iot hub device-identity show-connection-string --hub-name {YourIoTHubName} --device-id MyNodeDevice --output table
+CONNECTION_STRING = "{Your IoT hub device connection string}"
+
+# Using the MQTT protocol.
+PROTOCOL = IoTHubTransportProvider.MQTT
+MESSAGE_TIMEOUT = 10000
+
+# Define the JSON message to send to IoT Hub.
+TEMPERATURE = 20.0
+PRESSURE = 972.0
+HUMIDITY = 60
+MSG_TXT = "{\"temperature\": %.2f,\"pressure\": %.2f,\"humidity\": %.2f}"
+
+def send_confirmation_callback(message, result, user_context):
+    print ( "IoT Hub responded to message with status: %s" % (result) )
+
+def iothub_client_init():
+    # Create an IoT Hub client
+    client = IoTHubClient(CONNECTION_STRING, PROTOCOL)
+    return client
+#---------- Microsoft end ----------
+
+
+def iothub_client_telemetry_run():
+
+    try:
+        client = iothub_client_init()
+        print ( "IoT Hub device sending periodic messages, press Ctrl-C to exit" )
+
+        while True:
+
+            (chip_id, chip_version) = readBME280ID()
+            print "Chip ID     :", chip_id
+            print "Version     :", chip_version
+
+            temperature,pressure,humidity = readBME280All()
+
+            print "Temperature : ", (temperature * 9 / 5) + 32, "F"
+            print "Pressure : ", pressure, "hPa"
+            print "Humidity : ", humidity, "%"
+
+            # Build the message with simulated telemetry values.
+            ##temperature = TEMPERATURE + (random.random() * 15)
+            ##humidity = HUMIDITY + (random.random() * 20)
+            msg_txt_formatted = MSG_TXT % (temperature, pressure, humidity)
+            message = IoTHubMessage(msg_txt_formatted)
+
+            # Add a custom application property to the message.
+            # An IoT hub can filter on these properties without access to the message body.
+            prop_map = message.properties()
+            if temperature > 60:
+              prop_map.add("temperatureAlert", "true")
+            else:
+              prop_map.add("temperatureAlert", "false")
+
+            (chip_id, chip_version) = readBME280ID()
+            print "Chip ID     :", chip_id
+            print "Version     :", chip_version
+
+            # Send the message.
+            print( "Sending message: %s" % message.get_string() )
+            client.send_event_async(message, send_confirmation_callback, None)
+            time.sleep(10)
+
+    except IoTHubError as iothub_error:
+        print ( "Unexpected error %s from IoTHub" % iothub_error )
+        return
+    except KeyboardInterrupt:
+        print ( "IoTHubClient stopped" )
+
+
+if __name__ == '__main__':
+    print ( "Sending messages to Microsoft Azure IoT Hub" )
+    print ( "Press Ctrl-C to exit" )
+    iothub_client_telemetry_run()
+
